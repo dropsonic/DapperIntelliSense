329c687f7bbb08148f8fce62f33070f118a96f67
Vladimir Panchenko <vpanchenko@acumatica.com>
2019-09-03 16:00:28 +0300

    Add a sample project

---
+++ Samples/DapperTest/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using Dapper;
+
+namespace DapperTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	        using (IDbConnection connection = new SqlConnection("Data Source=(local);Initial Catalog=dapper;Integrated Security=True;"))
+	        {
+		        connection.Query<User>
+	        }
+        }
+    }
+
+	class User
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+
+	class Post
+	{
+		public int Id { get; set; }
+		public string Title { get; set; }
+		public string Content { get; set; }
+		public User Owner { get; set; }
+	}
+}
